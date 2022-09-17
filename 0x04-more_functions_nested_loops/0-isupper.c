9dc8b918ecdf5a44b6fbebe7bb69e1c032a91ec6
Olohitai Ojeikere <lohivie@gmail.com>
2022-09-16 22:00:14 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ *  _isuppe - chcks for ppercase characters
+ */
+int _isupper(int c)
+{
+	return(c >= 'A' && c <= "Z");
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+In thiss task we create more loops
